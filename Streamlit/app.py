07ca31cb67a59810ec9770da2198d9423a9fdd84
Giriankur <162001866+Giriankur@users.noreply.github.com>
2024-12-08 15:24:51 +0530

    Add files via upload

---
+++ Streamlit/app.py
@@ -0,0 +1,115 @@
+import streamlit as st
+
+#title
+
+st.title("welcome to my NLP Disaster dashboard")
+
+#Header/Subheader
+
+st.header("This is NLP Disaster Tweets and Non Disaster Tweets Project")
+
+#text
+
+st.text("Hellow streamlit")
+
+# Display Images
+
+# import Image from pillow to open images
+from PIL import Image
+img = Image.open("Project _07_Pic1.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Bar_chart_1.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+
+img = Image.open("Disaster_tweets and Non_Disaster_tweets.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Disaster_tweets and Non_disaster_tweets_1.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Keyword Frequency by Target Class.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+
+img = Image.open("Keyword Frequency by Target Class_1.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Pip_chart.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Pip_chart_1.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Top_10_key_word_V_shape.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Top 10 keyword_V_shape_1.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Word_cloud.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Word_cloud_1.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Word_lenght_disaster and Non_disaster.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("word_length disaster and Non disaster_1.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Confusion_matrix.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
+
+img = Image.open("Confusion_matrix_1.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img,width=800)
